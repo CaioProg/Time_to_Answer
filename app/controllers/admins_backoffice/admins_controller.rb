74fdd014ab676a6ca0f5f99b294239f99440e2ee
Caio Lucas <caioprog18@gmail.com>
2021-08-25 11:01:03 -0300

    Ajustando a rota Edit para o Admin

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -3,7 +3,7 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
     @admins = Admin.all
    end  
 
-   def edit 
-   # dgfsdgg
-   end
+  def edit 
+    @admin = Admin.find(params[:id])
+  end
 end
