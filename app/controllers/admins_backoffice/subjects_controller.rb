3fcb857796395abeacd2fa91077c4f3b543848c7
Caio Lucas <caioprog18@gmail.com>
2021-08-27 08:51:54 -0300

    Criando as views (new edit) para os Assuntos/Áreas

--- app/controllers/admins_backoffice/subjects_controller.rb
+++ app/controllers/admins_backoffice/subjects_controller.rb
@@ -1,8 +1,8 @@
 class AdminsBackoffice::SubjectsController < AdminsBackofficeController
-    before_action :set_subjects, only: [:edit, :update, :destroy]
+    before_action :set_subject, only: [:edit, :update, :destroy]
     
     def index
-      @subjects = Subject.all.page(params[:page])
+      @subjects = Subject.all.order(:description).page(params[:page])
      end  
   
      def new
@@ -10,7 +10,7 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
      end
   
      def create
-      @subject = Subject.new(params_subjects)
+      @subject = Subject.new(params_subject)
       if @subject.save
         redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área cadastrado com sucesso!"
       else
@@ -22,7 +22,7 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
     end
   
     def update  
-      if @subject.update(params_subjects)
+      if @subject.update(params_subject)
         redirect_to admins_backoffice_subjects_path, notice: "Assunto/Área atualizado com sucesso!"
       else
         render :edit
@@ -39,11 +39,11 @@ class AdminsBackoffice::SubjectsController < AdminsBackofficeController
   
     private
   
-    def params_admin
+    def params_subject
       params.require(:subject).permit(:description)  
     end
     
-    def set_subjects
+    def set_subject
       @subject = Subject.find(params[:id])
     end
   end
