90bad979a9dfaa36d33cedd03e79150ba97754f6
Caio Lucas <caioprog18@gmail.com>
2021-09-04 11:20:41 -0300

    Corrigindo o Nome do usuário

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
 
 
   # Validations
-  validates :first_name, presence: true, length: { minimum: 3}
+  validates :first_name, presence: true, length: { minimum: 3}, on: :update
 
 
   # Virtual Attributes
